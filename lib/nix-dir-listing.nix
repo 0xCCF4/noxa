0eec0f48c43fbc36ab85e805edc02fe0c39e6ae2
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2025-05-31 21:57:16 +0200

    Added library function: expand-path-list

--- lib/nix-dir-listing.nix
+++ lib/nix-dir-listing.nix
@@ -4,7 +4,7 @@
   ...
 }:
 with lib; with builtins;
-{
+rec {
 
   /**
       Generates a list of all `*.nix` files and `<name>/default.nix` files in the specified directory.
@@ -34,4 +34,29 @@ with lib; with builtins;
       nixFolders = filter (file: builtins.pathExists file) potentialNixFolders;
     in
     (nixFilesRoot ++ nixFolders);
+
+  /**
+      Generates a list of all `*.nix` files in the specified directory and its subdirectories; if a `path` is provided.
+      If a `[ Any ]` is provided, the input will be returned as is.
+      
+      # Inputs
+      `path` : The directory to search for Nix files.
+      
+      # Output
+      A list of paths in a directory or the input list of paths.
+
+      # Type
+      ```
+      Path -> [ Path ]
+      [ Any ] -> [ Any ]
+      ```
+      */
+  expand-path-list = paths: 
+    if typeOf paths == "path" then
+      list-nix-directory paths
+    else if typeOf paths == "list" then
+      paths
+    else
+      throw "expand-path-list expects a path or a list of paths";
+
 }
