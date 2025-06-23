10aa921999b956e7bcc4a90964429a5a32b229ee
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2025-06-23 08:55:08 +0200

    Fix: secret rekey dir not exist error message

--- modules/nixos/secrets/default.nix
+++ modules/nixos/secrets/default.nix
@@ -292,7 +292,7 @@ in
     assertions = with noxa.lib.ansi; [
       {
         assertion = length (attrNames config.age.secrets) > 0 -> (config.age.rekey.storageMode == "local" -> filesystem.pathIsDirectory config.age.rekey.localStorageDir);
-        message = "${bold+fgRed}The local storage directory for rekeying secrets ${fgCyan}'${config.age.rekey.localStorageDir}'${fgRed} does not exist. Did you run ${noBold}${fgCyan}'agenix rekey'${default}${bold}${fgRed} to create it?${default}";
+        message = "${bold+fgRed}The local storage directory for rekeying secrets ${fgCyan}'${toString config.age.rekey.localStorageDir}'${fgRed} does not exist. Did you run ${noBold}${fgCyan}'agenix rekey'${default}${bold}${fgRed} to create it?${default}";
       }
     ]
     ++
