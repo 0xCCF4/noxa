82987e51f62a04727a3c378ff6b5106f3daeb2b6
0xCCF4 <0xccf4-git-commit@lenzentech.de>
2025-07-23 11:09:22 +0200

    Style: code cleanup

--- modules/noxa/nodes/configuration.nix
+++ modules/noxa/nodes/configuration.nix
@@ -1,4 +1,27 @@
-{ lib, config, name, noxa, noxaConfig, agenix, agenix-rekey, ... }: with lib; with builtins; {
+{ lib, config, name, noxa, noxaConfig, agenix, agenix-rekey, ... }: with lib; with builtins; let
+  evalConfig = import (config.nixpkgs + "/nixos/lib/eval-config.nix") {
+    system = null;
+    modules = [
+      {
+        _module.args = {
+          inherit name;
+          noxaHost = name;
+          nodes = attrsets.mapAttrs (x: v: v.configuration) noxaConfig.nodes;
+        };
+      }
+      noxa.nixosModules.default
+    ];
+    specialArgs = {
+      inherit agenix;
+      inherit agenix-rekey;
+    } // config.specialArgs // {
+      noxa.lib = noxa.lib // {
+        nixpkgs = throw "Do not use the global nixpkgs of noxa. This is reserved for building the Noxa configuration.";
+      };
+    };
+  };
+in
+{
   options = with types; {
     specialArgs = mkOption {
       type = attrsOf (types.anything);
@@ -7,31 +30,7 @@
     };
 
     configuration = mkOption {
-      type =
-        let
-          evalConfig = import (config.nixpkgs + "/nixos/lib/eval-config.nix") {
-            system = null;
-            modules = [
-              {
-                _module.args = {
-                  inherit name;
-                  noxaHost = name;
-                  nodes = attrsets.mapAttrs (x: v: v.configuration) noxaConfig.nodes;
-                };
-              }
-              noxa.nixosModules.default
-            ];
-            specialArgs = {
-              inherit agenix;
-              inherit agenix-rekey;
-            } // config.specialArgs // {
-              noxa.lib = noxa.lib // {
-                nixpkgs = throw "Do not use the global nixpkgs of noxa. This is reserved for building the Noxa configuration.";
-              };
-            };
-          };
-        in
-        evalConfig.type;
+      type = evalConfig.type;
       description = "Nixos configuration for this node.";
     };
   };
