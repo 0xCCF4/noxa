80c748a2b21b180805154352aa5d611ffe91fcd5
0xCCF4 <0xCCF4@users.noreply.github.com>
2026-03-20 07:42:18 +0000

    ci: add: started writing architecture docs 232950cac42bfcae730033d013c028c59868ca94

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-64877cd0.js',
+                path_to_root + 'searchindex-d121992f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
